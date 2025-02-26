9db1bddd8ae58a18131354dd1b8fa236c4352770
BST_luo <1946831552@qq.com>
2025-02-26 09:35:24 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-
+  <div class="mainViewBox"></div>
 </template>
 
 <style scoped>
 
+.mainViewBox {
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+}
 </style>
